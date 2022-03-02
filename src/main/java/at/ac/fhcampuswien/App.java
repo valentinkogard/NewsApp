4c3bbc23ae97eba07c0ff85a33116241dfea91b2
gbk111 <92077726+gbk111@users.noreply.github.com>
2022-03-02 10:48:26 +0100

    displayMenu() done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,11 +1,24 @@
 package at.ac.fhcampuswien;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
     public String welcomeMessage(){
         return "Hello World!";
+    }
+
+    public String displayMenu(){
+
+        return "****************************** \n" +
+                "*  Welcome to NewsApp  * \n" +
+                "****************************** \n" +
+                "Enter what you want to do: \n"+
+                "a: Get top headlines Austria \n"+
+                "b: Get all news about bitcoin \n"+
+                "y: Count articles \n"+
+                "q: quit program\n";
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,5 +1,6 @@
 package at.ac.fhcampuswien;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +12,25 @@ public class AppTest {
 
         String actual = myApp.welcomeMessage();
         String expected = "Hello World!";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("displayMenuTest")
+    public void displayMenuTest() {
+
+        App myApp = new App();
+
+        String actual = myApp.displayMenu();
+        String expected = "****************************** \n" +
+                "*  Welcome to NewsApp  * \n" +
+                "****************************** \n" +
+                "Enter what you want to do: \n"+
+                "a: Get top headlines Austria \n"+
+                "b: Get all news about bitcoin \n"+
+                "y: Count articles \n"+
+                "q: quit program\n";
 
         assertEquals(expected, actual);
     }
