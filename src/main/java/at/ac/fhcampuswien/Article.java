0199c253ea516c3822384123768951d8bfca7f4d
Ehud321 <nikolaus.kapfer-rupp@stud.fh-campuswien.ac.at>
2022-03-02 11:18:34 +0100

    Modified Article Class

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -2,5 +2,27 @@ package at.ac.fhcampuswien;
 
 public class Article {
 
+    private String author = "";
 
+    private String title = "";
+
+    public void Atricle (String author, String title) {
+
+    }
+
+    public String getAuthor (String author) {
+        return author;
+    }
+
+    public String getTitle (String title) {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
